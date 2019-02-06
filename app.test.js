28f1efe59f2f09a2bf50b30679aa5a23caf5d7de
William Blasko <william@williamblasko.com>
2019-02-06 13:37:42 -0500

    prevent creation of duplicate shortened urls

--- app.test.js
+++ app.test.js
@@ -38,4 +38,23 @@ describe("URL Shortener", () => {
       .expect(301)
       .expect("Location", exampleUrl);
   });
+
+  test("Creating an already shortened URL will not create a duplicate", async () => {
+    const exampleUrl = "http://www.example.com";
+    const response = await request(app)
+      .post("/short_link")
+      .send({ long_url: exampleUrl })
+      .set("Accept", "application/json")
+      .expect(200);
+
+    expect(response.body.long_url).toBe(exampleUrl);
+
+    const secondResponse = await request(app)
+      .post("/short_link")
+      .send({ long_url: response.body.short_url })
+      .set("Accept", "application/json")
+      .expect(200);
+
+    expect(secondResponse.body.short_url).toBe(response.body.short_url);
+  });
 });
